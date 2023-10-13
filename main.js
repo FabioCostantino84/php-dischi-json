3190b902e44b70e421de3b4bd96909d68d3fb117
Fabio Costantino <fabiocostantino84@gmail.com>
2023-10-13 11:18:18 +0200

    add fixed get axios and fetch data

--- main.js
+++ main.js
@@ -1,9 +1,29 @@
-const { createApp } = Vue;
-
+const { createApp } = Vue
 createApp({
     data() {
         return {
-            title: 'verifica'
+
+            /* collego i due server */
+            //base_url: 'http://127.0.0.1:5173/server.php',
+            base_url: 'server.php',
+            songinfo: []
+
         }
     },
-}).mount('#app');
+
+    methods: {
+        fetchData(url) {
+            axios
+                .get(url)
+            then(response => {
+
+            })
+        }
+
+    },
+    mounted() {
+
+        this.fetchData(this.base_url);
+
+    }
+}).mount('#app')
